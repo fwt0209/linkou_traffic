f684665eb60786aa00c024450eb2db8691db0780
Kevin <fwt0209@gmail.com>
2021-03-30 01:08:13 +0800

    modify time

--- middleware/pagination.js
+++ middleware/pagination.js
@@ -25,7 +25,7 @@ function paginator(model) {
     let thisYear = localDateTime.getFullYear();
     let thisMonth = localDateTime.getMonth();
     let todayDate = localDateTime.getDate();
-    let passed1days = new Date(thisYear, thisMonth, todayDate);
+    let passed1days = new Date(thisYear, thisMonth, todayDate, +1);
 
     console.log(passed1days)
     try {
