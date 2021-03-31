f5d44669986f2de3f72e7adf2f946d3714bdf3a8
Kevin <fwt0209@gmail.com>
2021-04-01 00:14:02 +0800

    MODIFY TIME ZONE

--- middleware/pagination.js
+++ middleware/pagination.js
@@ -25,7 +25,7 @@ function paginator(model) {
     let thisYear = localDateTime.getFullYear();
     let thisMonth = localDateTime.getMonth();
     let todayDate = localDateTime.getDate();
-    let passed1days = new Date(thisYear, thisMonth, todayDate, -1);
+    let passed1days = new Date(thisYear, thisMonth, todayDate, -8);
 
     console.log(passed1days);
     try {
